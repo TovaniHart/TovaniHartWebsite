89751cf7f63712cb52da4a49bbc0ceae1f49b117
TovaniHart <todd@tovanihart.com>
2026-05-29 14:53:33 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -225,19 +225,17 @@ export const PROJECTS_INFO: ProjectProps[] = [
     media: [
       "Forma-construction.jpg",
       "Forma TI site plan test 20_40.jpg",
-      "Forma TI site plan test 30_60.jpg",
-      "Forma TI site plan test 30_60.jpg",
     ],
   },
   {
-    name: "Test CC",
-    location: "Tumwater, WA",
+    name: "FORMA Construction | Tenant Improvement",
+    location: "Olympia, WA",
     description:
-      "Tovani Hart worked closely with the Tumwater School District and the New Market Skills Center to design a new learning environment for the District's existing Options program.  The 14,500 sf Learning Center includes ten classrooms with high windows at both the interior and exterior walls.  This design element allows natural light to flood the classrooms, and the building interior, while visually screening the activities occurring beyond the walls.  The classrooms are organized around a dynamic circulation core with small gathering areas nestled beneath soft lower ceilings.  Creating a focused and secure space was a key factor during the design process.  The large sheltering roof protects a transparent main entry.  The control point just inside the main entry has a strong visual connection to the circulation area enhancing supervision and student safety.",
-    coverImage: "talc-285x285_web.jpg",
+      "Tovani Hart worked closely with FORMA Construction to design a new main office....",
+    coverImage: "forma_cover_72dpi-285px.jpg",
     media: [
-      "talc-rendering_web.jpg",
-      "talc-entry_web.jpg",
+      "Forma-construction.jpg",
+      "Forma TI site plan test 20_40.jpg",
       "talc-rendering_web.jpg",
       "talc-teaching-space_web.jpg",
     ],
