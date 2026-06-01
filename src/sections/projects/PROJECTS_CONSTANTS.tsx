0478ce008356a8387e88e418c67a216db3ed111e
TovaniHart <todd@tovanihart.com>
2026-06-01 09:49:59 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -52,7 +52,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Lacey, Washington",
     description:
       "Tovani Hart worked closely with NTPS through initial planning to purchase a vacant 80,000 square foot athletic facility and convert it into school district use. This multi-phased project began with a demolition phase to clean and secure the facility. A Temporary Use phase to support the adjacent high school modernization project through providing surge space and a third phase currently in process with design phase services to meet the needs of relocating New Aspire Middle School. This project has included complex master planning including boundary line adjustments and dedication of rights-of-way and persistent involvement with the City of Olympia through complicated changes in how the project is to be permitted. Project highlights include high school competition gymnasium, impressive circulation corridor and the expressed existing, reinforced structure. Phase III will be constructed while students occupy the temporary use space.",
-    coverImage: "ntps-thumb.jpg",
+    coverImage: "asipre_cover_remove.jpg",
     media: [
       "aspire-ext-1.jpg",
       "aspire-detail.jpg",
@@ -64,7 +64,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     ],
   },
   {
-    name: "Olympia Fireplace and Spa",
+    name: "Well 80 | Former Olympia Fireplace and Spa",
     location: "Olympia, Washington",
     description:
       "After a fire burned a zero lot line building in the historic district of downtown Olympia, the building owner sought to re-build the structure. The Owner envisioned continued use as a warehouse with the potential for future retail, or business tenants. Tovani Hart developed a design allowing the current tenant to easily operate, all while giving the street presence of a retail establishment. This building grants the owner flexibility for potential conversion of the space for retail, office, services, or restaurant. The brick façade and entry alcove provide a strong presence and measured relief from the existing 4th Avenue street wall, while façade details, awnings, and the color pallet compliment the historical context of downtown. Interior exposed barrel vault trusses with raw steel connections express the original structure which burned down.",
@@ -160,7 +160,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Olympia, Washington",
     description:
       "In June of 2017, Tovani Hart was invited to participate in the design of a seven story condominium building in downtown Olympia, Washington.  Sited directly across from Percival Landing Park in the heart of the City, the condominiums are adjacent to several well-established businesses including restaurants, night clubs, along with Olympia’s Community Center.  Capitalizing on the natural amenities of the location, the building opens to views on all sides and includes unobstructed vistas of Budd Inlet and the Olympic Peninsula.  The 28 unit building provides a variety of plan options ranging from one bedroom plus a den at approximately 1,300 square feet, to two bedrooms plus a den with approximately 2,370 square feet.  The first two levels include lobby access and a secure private parking structure for residents.  Each unit is designed with open concept great rooms, expansive views and a large balcony.",
-    coverImage: "percival_ext-render-tumbnail.jpg",
+    coverImage: "percival_cover_remove.jpg",
     media: [
       "percival_ext-render.jpg",
       "percival-3-6-flr-plan.jpg",
@@ -194,7 +194,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Olympia, Washington",
     description:
       "This residential project is within an established Olympia neighborhood nestled at the end of the cul-de-sac. The first phase of work focused on necessary home repairs to the siding, roofing and existing wood burning fireplace, and chimney. In process of residing the exterior the Owner made a decision to express the design elements of their Colonial home by incorporating functional shutters, inlaid brickwork in walkways and patios, expressive exterior trim and a new covered porch with copper detailing. The second phase included an extensive first floor remodel of the foyer, kitchen, dining room, laundry, guest bath, and family rooms. Originally there were several doorways to access the kitchen, dining, and laundry rooms resulting in expansive circulation space and a very small kitchen, the remodel yielded a large open kitchen with peninsula that opens to the adjacent family room. The new office space supports desk area for the entire family including a small sitting area.",
-    coverImage: "owens-thumb.jpg",
+    coverImage: "owens_cover_remove.jpg",
     media: [
       "owens_alt.jpg",
       "owens-entry-render.jpg",
