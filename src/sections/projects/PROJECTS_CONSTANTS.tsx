a176d5434dbb34f296f4e2d111162dd07c3f90ad
TovaniHart <todd@tovanihart.com>
2026-06-01 11:34:28 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -251,7 +251,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Olympia, WA",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    coverImage: "avanti_cover",
+    coverImage: "avanti_cover.jpg",
     media: [
       "avanti_Entrance.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
