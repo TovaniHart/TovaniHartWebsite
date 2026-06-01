cdd29f7b6f562eaf94d369ac8020080b863d12f4
TovaniHart <todd@tovanihart.com>
2026-06-01 13:54:19 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -267,7 +267,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
     coverImage: "baytown_fire_training.jpg",
     media: [
       "baytown_fire.jpg",
-      "Baytown Phase III 30 transparency_251 site.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
@@ -356,9 +356,9 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Lacey, WA",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    coverImage: "lacey_maker_cover.jpg",
+    coverImage: "Cover Image_In Progress_72dpi-285px.jpg",
     media: [
-      "lacey_maker_space.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
