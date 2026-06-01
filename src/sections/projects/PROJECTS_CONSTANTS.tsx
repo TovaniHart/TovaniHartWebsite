c70d75ca49869b1797296108384b2b1a989ac5bb
TovaniHart <todd@tovanihart.com>
2026-06-01 12:21:29 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -348,6 +348,45 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
+          ],
+  },
+  {
+    name: "Saint Martin's University | Lacey Maker Space",
+    location: "Lacey, WA",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    coverImage: "Cover Image_In Progress_72dpi-285px.jpg",
+    media: [
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+    ],
+  },
+  {
+    name: "Saint Martin's University | Nursing Building",
+    location: "Lacey, WA",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    coverImage: "smu_nursing_cover.jpg",
+    media: [
+      "smu_nursing.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+    ],
+  },
+  {
+    name: "Saint Martin's University | Science Building",
+    location: "Lacey, WA",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    coverImage: "smu_science_cover.jpg",
+    media: [
+      "smu_science_render.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
+      "Cover Image_In Progress_72dpi-285px.jpg",
     ],
   },
 ];
