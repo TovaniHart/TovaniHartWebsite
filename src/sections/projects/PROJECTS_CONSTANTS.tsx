e78b23115250f25ed47ee51fb5c6679cb770c1c9
TovaniHart <todd@tovanihart.com>
2026-06-01 13:04:22 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -228,7 +228,6 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "Forma-construction.jpg",
       "Forma TI site plan test Grey Scale exist 30 transparency_251 site.jpg",
       "Forma TI site plan test Grey Scale 30 transparency.jpg",
-      "Forma TI site plan test Grey Scale 1.jpg",
       "ADNA ES with 40 site transparency.jpg",
       "ADNA ES with Existing and New transparency 20 existing and 40 site.jpg",
     ],
