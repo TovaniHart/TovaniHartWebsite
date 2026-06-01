756f7d1eb020c2c9cc270b8cd758c1427dbbe80d
TovaniHart <todd@tovanihart.com>
2026-06-01 15:39:03 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -411,19 +411,6 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
     ],
-   },
-  {
-    name: "Timberline High School | New PE Facility",
-    location: "Lacey, WA",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    coverImage: "Cover Image_In Progress_72dpi-285px.jpg",
-    media: [
-      "Cover Image_In Progress_72dpi-285px.jpg",
-      "Cover Image_In Progress_72dpi-285px.jpg",
-      "Cover Image_In Progress_72dpi-285px.jpg",
-      "Cover Image_In Progress_72dpi-285px.jpg",
-    ],
   },
 ];
 
