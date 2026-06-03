cf1e2304c9620f629c030b3798fae5b58a9a7124
TovaniHart <todd@tovanihart.com>
2026-06-03 16:13:44 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -45,7 +45,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "609_columbia_plan.jpg",
       "609-door-sketch.jpg",
       "609_elevation_72dpi_1400px.jpg",
-      "609_doors_construction_72dpi_1400px.jpg"
+      "609_doors_construction_72dpi_1400px.jpg",
       "609-const-2.jpg",
       "609-int-1.jpg",
     ],
