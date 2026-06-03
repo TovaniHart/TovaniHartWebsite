3c4a1f07fb957e73dcfff1a0f033d143ec7a6063
TovaniHart <todd@tovanihart.com>
2026-06-03 15:08:22 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -177,10 +177,10 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "talc_ceiling_72dpi_1400px.jpg",
       "talc_teaching_space_72dpi_1400px.jpg",
       "talc_classroom_72dpi_285px.jpg",
+      "talc_bike_rack_72dpi_1400px.jpg",
       "talc_exterior_entry_72dpi_1400px.jpg",
       "talc_exterior_corner_72dpi_1400px.jpg",
       "talc_parking_72dpi_285px.jpg",
-      "talc_bike_rack_72dpi_1400px.jpg",
     ],
    },
   {
