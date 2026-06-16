5292b8115b8a19efcd2e2dd3bc3fb68c2eb1ce98
TovaniHart <todd@tovanihart.com>
2026-06-16 10:06:35 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -204,16 +204,6 @@ export const PROJECTS_INFO: ProjectProps[] = [
     coverImage: "forma_cover_72dpi-285px.jpg",
     media: [
       "forma_cover_72dpi-1400px.jpg",
-      "forma_ti_plan.jpg",
-      "forma_programming_72dpi-1400px.jpg",
-      "forma_sketch_72dpi-285px.jpg",
-      "forma_model_72dpi-285px.jpg",
-      "forma_reception_72dpi-1400px.jpg",
-      "forma_office_72dpi-1400px.jpg",
-      "forma_kitchenette_72dpi-1400px.jpg",
-      "forma_lighting_72dpi-1400px.jpg",
-      "forma_entry_72dpi-1400px.jpg",
-      "forma_construction_72dpi-1400px.jpg",
     ],
    },
   {
