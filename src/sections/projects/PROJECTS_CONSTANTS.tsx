1dd7914e747a49ad13bd45d6dfdf7c68aca4db13
TovaniHart <todd@tovanihart.com>
2026-06-17 09:53:16 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -278,10 +278,11 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Buckley, WA",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    coverImage: "buckley_cover.jpg",
+    coverImage: "buckley_cover_72dpi_285px.jpg",
     media: [
-      "buckley_construction.jpg",
+      "buckley_cover_72dpi_1400px.jpg",
       "buckley_city_hall_plan_72dpi_1400px.jpg",
+      "buckley_city_hall_construction_72dpi_1400px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
     ],
