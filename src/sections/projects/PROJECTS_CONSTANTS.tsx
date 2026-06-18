2446413af9678af5e7fade4a6d3dd173776791e1
TovaniHart <todd@tovanihart.com>
2026-06-18 13:41:22 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -476,9 +476,9 @@ export const PROJECTS_INFO: ProjectProps[] = [
     location: "Spring, TX",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    coverImage: "spring_cover.jpg",
+    coverImage: "spring_cover_72dpi_285px.jpg",
     media: [
-      "Spring_aerial.jpg",
+      "spring_aerial_72dpi_1400px.jpg",
       "spring_ftc_plan_72dpi_1400px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
       "Cover Image_In Progress_72dpi-285px.jpg",
