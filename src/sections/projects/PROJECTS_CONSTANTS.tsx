423f11da804365b40f654422200c5f6f2bc9bae8
TovaniHart <todd@tovanihart.com>
2026-06-19 09:37:33 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -470,7 +470,7 @@ export const PROJECTS_INFO: ProjectProps[] = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     coverImage: "spring_cover_72dpi_285px.jpg",
     media: [
-      "spring_aerial_72dpi_1400px.jpg",
+      "spring_aerial_a_72dpi_1400px.jpg",
     ],
    },
   {
