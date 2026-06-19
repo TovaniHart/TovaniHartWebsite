031d387818da3937b3f803729ede3fb5aa716170
TovaniHart <todd@tovanihart.com>
2026-06-19 14:54:59 -0700

    Update PROJECTS_CONSTANTS.tsx

--- src/sections/projects/PROJECTS_CONSTANTS.tsx
+++ src/sections/projects/PROJECTS_CONSTANTS.tsx
@@ -70,7 +70,8 @@ export const PROJECTS_INFO: ProjectProps[] = [
   {
     name: "Waterfront Residence",
     location: "Olympia, Washington",
-    description: "text text text",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     coverImage: "waterfront-residence_72dpi-285px.jpg",
     media: [
       "waterfront-residence-horz.jpg",
